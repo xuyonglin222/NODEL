f77132185caf42a2e63f7dbf355fc00a334254c0
xuyonglin222 <996726447@qq.com>
2017-12-30 01:04:52 +0800

    add

--- albumServer/load_albums.js
+++ albumServer/load_albums.js
@@ -18,10 +18,12 @@ function load_album_list(callback){
             else{
                 fs.stat('albums/'+files[i],function(err,stat){
                     if(err){
-                        throw err;
+                        allback(make_error("file_error",JSON.stringify(err)));
+                        return;
                     }
                     if(stat.isDirectory()){
-                        only_dirs.push(files[i]);
+                        var obj = {name:files[i]};
+                        only_dirs.push(obj);
                     }
                     iterator(i+1);
                 });
@@ -33,24 +35,105 @@ function load_album_list(callback){
 //网络通讯
 function handle_incoming_request(req,res){
     console.log("INCOMING REQUEST: "+ req.method +" "+req.url);
+    if(req.url == '/albums.json'){
+        handle_list_albums(req,res);
+    }
+    else if(req.url.substr(0,7) == '/albums' && req.url.substr((req.url.length - 5) == '.json')){
+        handle_get_album(req,res);
+    }else{
+        send_failure(res,404,invalid_resource());
+    }
+}
+function handle_list_albums(req,res){
     load_album_list(function(err,albums){
         if(err){
-            res.writeHead(503,{"Content-Type":"application/json"});
-            res.end(JSON.stringify(err)+"\n");
+            send_failure(res,500,err);
             return;
         }
-
-        var out={
-            error:null,
-            data:{
-                albums:albums
+        send_success(res,{albums:albums});
+    });
+}
+function load_album(album_name,callback){
+    //we will just assume that any directory in our 'albums'
+    //subfolder is an album.
+    fs.readdir('albums/'+album_name,function(err,files){
+        if(err){
+            if(err.code == "ENOENT"){
+                callback(no_such_album());
+            }else{
+                callback(make_error("file_error",JSON.stringify(err)));
             }
-        };
-        res.writeHead(200,{"Content-Type":"application/json"});
-        res.end(JSON.stringify(out)+"\n");
+            return;
+
+        }
+
+        var only_files = [];
+        var path = "albums/"+album_name +'/';
+        (function iterator(index){
+            if(index ==files.length){
+                var obj = {short_name:album_name,photos:only_files};
+                callback(null,obj);
+                return;
+            }
+            fs.stat(path+files[index],function(err,stat){
+                if(err){
+                    callback(make_error("file_error",JSON.stringify(err)));
+                    return;
+                }
+                if(stat.isFile()){
+                    var obj = {
+                        filename:files[index],
+                        desc:files[index]
+                    };
+                    only_files.push(obj);
+                }
+                iterator(index+1);
+            })
+        })(0);
+
     })
 }
 
+function handle_get_album(req,res){
+    var album_name = req.url.substr(7,req.url.length-12);
+    console.log("album_name: "+album_name);
+    load_album(album_name,function(err,album_contents){
+        if(err&&err.error == "no_such_album"){
+            send_failure(res,404,err);
+            return;
+        }
+        else if(err){
+            send_failure(res,500,err);
+            return;
+        }else{
+            send_success(res,{album_data:album_contents});
+        }
+    });
+}
+
+function make_error(err,msg){
+    var e =new Error(msg);
+    e.code = err;
+    return e;
+}
+
+function send_success(res,data){
+    res.writeHead(200,{"Content-Type":"application/json"});
+    var output = {error : null,data:data};
+    res.end(JSON.stringify(output)+"\n");
+}
+function send_failure(res,code,err){
+    var code = (err.code)?code:err.name;
+    console.log(code)
+    res.writeHead(code,{"Content-Type":"application/json"});
+    res.end(JSON.stringify({error:code,message:err.message}) + "\n");
+}
+function invalid_resource(){
+    return make_error("invalid_resource","the requested resourse does not exist");
+}
+function no_such_album(){
+    return make_error("no_such_album","The specified album does not ecist");
+}
 var s = http.createServer(handle_incoming_request);
 s.listen(8080,function(){
     console.log("server is running on 8080");
